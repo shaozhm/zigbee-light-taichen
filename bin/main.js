d970768b30f4d44f9abcaaef430f9cadd67591bd
Shao, Zhimin <zhimin.shao@sap.com>
2025-03-30 14:07:21 +0800

    update

--- bin/main.js
+++ bin/main.js
@@ -43,6 +43,11 @@ topics.forEach((topic) => {
 let disableSensor1 = false;
 let keepLight1 = true;
 
+// 厕所的全局变量定义
+let disableSensor2 = false;
+let keepLight2 = true;
+let door2contact = false;
+
 client.on('message', function(topic, message) {
   console.log(`[${topic}] message: `, message.toString());
   console.log(`KeepLight-1/Disable Sensor 1: `, keepLight1, disableSensor1);
@@ -119,6 +124,60 @@ client.on('message', function(topic, message) {
           }
         })
       }
+    }
+
+    // 主卧的床前总控开关
+    if ((topic.endsWith('button-1') || topic.endsWith('button-2')) && mesgJSON) {
+      const { action } = mesgJSON;
+      const sendMesg = action === 'single' ? '{"state": "ON"}' : action === 'double' || action ==='triple' || action === 'quadruple' ? '{"state": "OFF"}' : null
+      if (sendMesg) {
+      }
+    }
+
+    // keepLight变量: 表示目前希望开/关灯，但关灯会延迟几秒再执行，当延迟时间到时，执行前需检测 keepLight 变量是否还是 false. 
+    // 到那时如果还是 false 则需要关灯，否则不需要执行。
+
+    // 延迟开启厕所的人体红外感应器
+    const fnDisableSensor2Off = () => {
+      if (!keepLight2) {
+        disableSensor2 = false;
+        console.log(`Disable Sensor 2 set to: `, disableSensor2);
+      }
+    };
+
+    // 厕所的主开关
+    if (checkTopicProperty('ikea-styrbar-g-2', 'action')) {
+      if (mesgJSON.action === 'on') {
+        keepLight2 = true;
+        disableSensor2 = true;
+        console.log(`Disable Sensor 2 set to: `, disableSensor2);        
+      }
+      if (mesgJSON.action === 'off') {
+        keepLight2 = false;
+        setTimeout(fnDisableSensor2Off, 3000);
+      }
+    }
+
+    // 厕所的门感应器
+    if (checkTopicProperty('door-2', 'contact')) {
+      door2contact = mesgJSON.contact;
+      console.log('door-2 contact', door2contact);
+      // 如果门关着，人体红外感应器要停止检测。
+      keepLight2 = mesgJSON.contact;
+      disableSensor2 = mesgJSON.contact;
+      console.log(`Disable Sensor 2 set to: `, disableSensor2);
+    }
+
+    // 厕所的人体红外感应器
+    if (checkTopicProperty('move-sensor-2', 'occupancy') && !disableSensor2) {
+      const sendMesg = mesgJSON.occupancy ? '{ "state": "ON" }' :  '{ "state": "OFF" }';
+      const topic = 'zigbee2mqtt/bathroom/set';
+      console.log(`Send Topic: ${topic}, Message: ${sendMesg}`);
+      client.publish(topic, sendMesg, { qos: 0, retain: false }, (error) => {
+        if (error) {
+          console.error(error)
+        }
+      })
     }
   }
 });
